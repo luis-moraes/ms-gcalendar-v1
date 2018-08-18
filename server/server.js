245b8da67190f7f4d99a2c16b104b3f10497345a
Luis Carlos de Moraes Junior <luis.junior@cielo.com.br>
2018-08-18 16:43:52 -0300

    get calendar

--- server/server.js
+++ server/server.js
@@ -19,6 +19,31 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.get('/calendar/:clinicName', (req, res) => {
+  
+  var cName = req.params.clinicName;
+  
+
+  if (!cName) {
+    return res.status(404).send();
+  }
+
+  Calendar.findOne({'clinicName' : cName}).then((calendar) => {
+    var calId = calendar.gcalId;
+
+    var resp = {
+      gcalendarOwnerLink : `https://calendar.google.com/calendar/r?cid=${calId}`,
+      gcalendarPublicLink : `https://calendar.google.com/calendar/embed?src=${calId}&ctz=America%2FSao_Paulo`
+    }
+
+    res.send(resp);
+
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  
+});
+
 app.post('/calendar', (req, res) => {
   var calendar = new Calendar({
     clinicName: req.body.clinicName,
